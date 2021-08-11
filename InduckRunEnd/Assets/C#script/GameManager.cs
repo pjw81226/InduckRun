21f87f18eb81816ada9b64204fa6c50879fd0a7d
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2021-08-11 21:30:04 +0900

    END

---
+++ InduckRunEnd/Assets/C#script/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int[] foodGoldList;
+    public Sprite[] TrashSprite;
+
+    public Sprite[] Background;
+
+    public Sprite[] Preview;
+    public int[] InduckID;
+    public int[] InduckGold;
+    public string[] InduckName;
+    public Sprite[] InduckSprite;
+    public int[] ClothesGold;
+
+    public Vector3[] PointList;
+    public Vector3[] TrashPoint;
+}
